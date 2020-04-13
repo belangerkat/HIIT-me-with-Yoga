016e4aab77814c25759358692d6ae26080c40d01
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-04-13 17:59:46 -0400

    created backEnd and frontEnd

---
+++ frontEnd/app.js
@@ -0,0 +1,9 @@
+console.log("yoga time")
+
+// function websiteLogo() {
+//     const div = document.querySelector('#logo')
+//     const logo = document.createElement('img')
+//     logo.src = 'images/logo.png'
+//     div.appendChild(logo)
+// }
+// websiteLogo()
