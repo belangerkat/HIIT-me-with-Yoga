5ed2bcfece0bcdee0b5747115a7a9ddfa5fcd734
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-04-14 14:38:08 -0400

    added auth to frontend

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,5 +1,34 @@
-console.log("yoga time")
+const loginForm = document.querySelector('.login')
 
+loginForm.addEventListener('submit', handleLogin)
+
+function handleLogin(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const username = formData.get('username')
+    const password = formData.get('password')
+    const loginBody = {username, password}
+    
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginBody)
+    }).then(response => response.json())
+    .then(result => {
+        console.log(result)
+        localStorage.setItem("token", result.token)
+    })
+    
+    fetch("http://localhost:3000/users", {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.token}`
+        }
+    })
+    event.target.reset()
+}
 // function websiteLogo() {
 //     const div = document.querySelector('#logo')
 //     const logo = document.createElement('img')
